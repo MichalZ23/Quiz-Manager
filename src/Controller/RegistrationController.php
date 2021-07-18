cce31ebda2a3faf5b6cd54a74095369805aed707
MichalZ23 <mich.zawalich@gmail.com>
2021-07-19 00:44:10 +0200

    added authentication

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'register')]
+    public function register(Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User;
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            $user = $form->getData();
+            $user->setPassword(
+                $hasher->hashPassword($user, $user->getPassword())
+            );
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            dump($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('app_login'));
+        }
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
